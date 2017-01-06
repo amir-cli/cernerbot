d0045de9b7db885fd800b95cdb528fca3edd5036
amir-cli <cernerteam8@gmail.com>
2017-01-06 12:00:50 +0330

    Update Support.lua

--- plugins/Support.lua
+++ plugins/Support.lua
@@ -5,12 +5,12 @@
 ]]
 do
     local function run(msg, matches)
-    local support = '1050575893'
+    local support = '1032404398'
     local data = load_data(_config.moderation.data)
     local name_log = user_print_name(msg.from)
         if matches[1] == 'support' or 'tosupport' then
         local group_link = data[tostring(support)]['settings']['set_link']
-    return " <i>Link</i> : \n"..group_link.."\n\n<code>Channel</code>\n<i>By CerNer Team</i>"
+    return " <i>Link</i> : \n"..group_link.."\n\n<code>Channel</code> @CerNerCH\n<i>By CerNer Team</i>"
     end
 end
 return {
