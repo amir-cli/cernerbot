3bb847ba6c7a0c9aac2feb59c2ce4f8c5dc8b0da
amir-cli <cernerteam8@gmail.com>
2017-01-06 12:02:17 +0330

    Update setsudo.lua

--- plugins/setsudo.lua
+++ plugins/setsudo.lua
@@ -3,7 +3,7 @@ function reload_plugins( )
   load_plugins() 
 end 
    function run(msg, matches) 
-    if tonumber (msg.from.id) == 329467212 then--expample 123456789 
+    if tonumber (msg.from.id) == 1032404398 then--expample 123456789 
        if matches[1]:lower() == "setsudo" then 
           table.insert(_config.sudo_users, tonumber(matches[2])) 
       print(matches[2]..' added to sudo users') 
@@ -26,5 +26,5 @@ patterns = {
 }, 
 run = run 
 } 
---By REZA 
---Our Channel : http://Telegram.me/Hextor_Ch 
+--By amir 
+--Channel @CerNerCH
