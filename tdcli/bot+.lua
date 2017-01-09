12a10cf1582262836732d29306c21c166ec8d044
amir-cli <cernerteam8@gmail.com>
2017-01-09 13:14:12 +0330

    Update bot+.lua

--- tdcli/bot+.lua
+++ tdcli/bot+.lua
@@ -200,9 +200,8 @@ function tdcli_update_callback(data)
         tdcli.sendText(chat_id, msg.id_, 0, 1, nil, '<b>PONG</b>', 1, 'html')
       end
       if input:match("^[Ii][Dd]$") then
-        tdcli.sendText(chat_id, msg.id_, 0, 1, nil, '<b>SuperGroup ID : </b><code>'..string.sub(chat_id, 5,14)..'</code>\n<b>User ID : </b><code>'..user_id..', 1, 'html')
+       tdcli.sendText(chat_id, msg.id_, 0, 1, nil, '<b>SuperGroup ID : </b><code>'..string.sub(chat_id, 5,14)..'</code>\n<b>User ID : </b><code>'..user_id..'</code>\n<b>Channel : </b>@CerNerCH', 1, 'html')
       end
-
       if input:match("^[Pp][Ii][Nn]$") and reply_id and is_owner(msg) then
         tdcli.sendText(chat_id, msg.id_, 0, 1, nil, '<b>Message Pinned</b>', 1, 'html')
         tdcli.pinChannelMessage(chat_id, reply_id, 1)
